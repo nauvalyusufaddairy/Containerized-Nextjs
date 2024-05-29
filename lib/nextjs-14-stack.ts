72fbbd396e83ee6c82bae52db546daf0b0eeb707
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2024-05-29 22:23:22 +0700

    initial

--- lib/nextjs-14-stack.ts
+++ lib/nextjs-14-stack.ts
@@ -1,5 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Nextjs14Stack extends cdk.Stack {
